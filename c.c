72a535d57d6261cb4d71bac794ec53419c6a0fda
HackieMesser <41595086+HackieMesser@users.noreply.github.com>
2023-02-06 10:41:07 +0100

    Update c.c

--- c.c
+++ c.c
@@ -3,3 +3,15 @@ int main(){
     printf("connor first prog in c \n");
     
     return 0;
+
+    
+    
+#include <stdio.h>
+
+int main()
+{
+    int num = 15; 
+    printf("%i", num);
+
+    return 0;
+}
